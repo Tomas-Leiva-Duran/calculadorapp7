83c6a3d11bb19fa876d7b897759f8dc2a3082013
Tomas-Leiva-Duran <101905549+Tomas-Leiva-Duran@users.noreply.github.com>
2024-01-19 14:05:25 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.calculadorapp7',
+  appName: 'calculadorapp7',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
